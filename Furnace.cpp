8ef02b9d3b0596076d28178d461c4ae395cf0ef0
Fasmaret <35273234+Fasmaret@users.noreply.github.com>
2022-04-01 21:36:22 +0200

    Update Furnace.cpp

--- Furnace.cpp
+++ Furnace.cpp
@@ -1,8 +1,18 @@
+#include "Furnace.h"
 
+// (вход, установка, п, и, д, период в секундах, мин.выход, макс. выход)
+// PID функция
+int computePID(float input, float setpoint, float kp, float ki, float kd, float dt, int minOut, int maxOut) {
+  float err = setpoint - input;
+  static float integral = 0, prevErr = 0;
+  integral = constrain(integral + (float)err * dt * ki, minOut, maxOut);
+  float D = (err - prevErr) / dt;
+  prevErr = err;
+  return constrain(err * kp + integral + D * kd, minOut, maxOut);
+}
 
-
-
-void messure () {
+void messure(LiquidCrystal_I2C lcd, MAX6675 thermocouple_1, MAX6675 thermocouple_2, MAX6675 thermocouple_3) 
+{
 int T1 = thermocouple_1.readCelsius();
 int T2 = thermocouple_2.readCelsius();
 int T3 = thermocouple_3.readCelsius();
