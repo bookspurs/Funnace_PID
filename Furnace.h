7e84d5b01b76b8c00d37c6448ab15b04d36f03a0
Fasmaret <35273234+Fasmaret@users.noreply.github.com>
2022-04-01 21:36:20 +0200

    Update Furnace.h

--- Furnace.h
+++ Furnace.h
@@ -1,2 +1,8 @@
+#ifndef FURNACE_H
+#define FURNACE_H
+#include <Arduino.h>
+#include <max6675.h>
+#include <LiquidCrystal_I2C.h>
+void messure(LiquidCrystal_I2C lcd, MAX6675 thermocouple_1, MAX6675 thermocouple_2, MAX6675 thermocouple_3);
 
-void messure ();
+#endif
